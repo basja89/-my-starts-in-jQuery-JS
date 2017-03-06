09ca47e9f31e23e4567ccef0cefeda9856c4ab68
basja89 <baska.wierzbicka@gmail.com>
2017-03-06 10:57:52 +0100

    my first tasks
    in JS/jQuery/Ajax

---
+++ carousel with jQuery/js/scripts.js
@@ -0,0 +1,17 @@
+var carouselList = $("#carousel .slides");
+
+function moveFirstSlide() {
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+	lastItem.after(firstItem);
+	carouselList.css({
+		marginLeft: 0
+	});
+};
+
+function changeSlide() {
+	carouselList.animate({
+		marginLeft: -400
+	}, 500, moveFirstSlide);
+};
+setInterval(changeSlide, 3000);
